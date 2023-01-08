54f648412d59abb579f96ae8642db46056e8b019
Ihor Zakharov <nograveoff@gmail.com>
2023-01-08 14:20:16 +0200

    creating link to register page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:photogram/widgets/login_button.dart';
 import 'package:photogram/widgets/login_form.dart';
+import 'package:photogram/widgets/register_page_link.dart';
 import 'package:photogram/widgets/title_widget.dart';
 
 class LoginPage extends StatelessWidget {
@@ -22,9 +23,10 @@ class LoginPage extends StatelessWidget {
               mainAxisSize: MainAxisSize.max,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                TitleWidget(),
+                const TitleWidget(),
                 LoginForm(loginFormKey: loginFormKey),
                 LoginButton(loginFormKey: loginFormKey),
+                RegisterPageLink(),
               ],
             ),
           ),
---
+++ lib/widgets/register_page_link.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'package:photogram/pages/register_page.dart';
+
+class RegisterPageLink extends StatelessWidget {
+  const RegisterPageLink({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const RegisterPage(),
+        ),
+      ),
+      child: const Text(
+        'Don\'t have an account?',
+        style: TextStyle(
+          color: Colors.blue,
+          fontSize: 15,
+          fontWeight: FontWeight.w200,
+        ),
+      ),
+    );
+  }
+}
