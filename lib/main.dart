5cacae54f22d1c5dd6005b2ead775c152f127e72
Ihor Zakharov <nograveoff@gmail.com>
2023-01-15 19:12:04 +0200

    added firebase service

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get_it/get_it.dart';
 
 import 'package:photogram/photogram_app.dart';
+import 'package:photogram/services/firebase_service.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  GetIt.instance.registerSingleton<FirebaseService>(FirebaseService());
   runApp(const PhotogramApp());
 }
