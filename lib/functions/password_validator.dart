d57c183679457b4c9308d5a8845d3853e0bcaa88
Ihor Zakharov <nograveoff@gmail.com>
2023-02-12 17:37:23 +0200

    Add Regex for password

--- lib/functions/password_validator.dart
+++ lib/functions/password_validator.dart
@@ -1,6 +1,10 @@
 String? passwordValidator(String? password) {
-  if (password != null && password.length > 6) {
+  if (password != null && password.contains(
+      // Minimum eight characters, at least one uppercase letter,
+      // one lowercase letter and one number
+      RegExp(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$"))) {
     return null;
   }
-  return 'Please enter a password greater than 6 characters';
+  return 'Please enter a password with minimum eight characters, at least one '
+      'uppercase letter, one lowercase letter and one number';
 }
